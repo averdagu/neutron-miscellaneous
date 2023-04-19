9ad4b7962963261ee6f7461b947248a540d8ab6b
Arnau Verdaguer <averdagu@redhat.com>
2023-04-19 16:36:28 +0200

    small logs accept name as argument

--- small-logs.py
+++ small-logs.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 import sys
 
+name="server.log"
+
+if len(sys.argv) > 1:
+    name=sys.argv[1]
+
 # Open a file
-with open("server.log", "r+") as fin, open("server.log.min", "w") as fout:
+with open(name, "r+") as fin, open(name+".min", "w") as fout:
     for l in fin.readlines():
         if '[' in l and ']' in l:
             l = l[:l.find('[')+1] + l[l.find(']'):]
